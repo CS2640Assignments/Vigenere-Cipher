f9b920e30e9eb669088263b3e05e11c4d2e08749
joestrada1022 <student.joshuaestrada@gmail.com>
2023-11-29 20:14:52 -0800

    cleaned up code

--- utils.asm
+++ utils.asm
@@ -1,52 +1,52 @@
 .macro getKey
-printStr(keyPrompt)
-li $v0, 8
-la $a0, buffer
-la $a1, 50
-syscall
+	printStr(keyPrompt)
+	li $v0, 8
+	la $a0, buffer
+	la $a1, 50
+	syscall
 .end_macro
 
 .macro getFile
 prompt:
-    # Prompt for file name
-    printStr(filePrompt)
+    	# Prompt for file name
+    	printStr(filePrompt)
 
-    # Read file name from user
-    li $v0, 8
-    la $a0, fin
-    li $a1, 25
-    syscall
+    	# Read file name from user
+    	li $v0, 8
+    	la $a0, fin
+    	li $a1, 25
+    	syscall
 	la $t0, fin
 	process($t0)
 open_file:
-    # Open the file for reading
-    li $v0, 13
-    la $a0, fin
-    li $a1, 0
-    li $a2, 0
-    syscall
-    move $s1, $v0  # Save the file descriptor
+    	# Open the file for reading
+    	li $v0, 13
+    	la $a0, fin
+    	li $a1, 0
+    	li $a2, 0
+    	syscall
+    	move $s1, $v0  # Save the file descriptor
 
-    # Check if file opened successfully
-    bltz $s1, file_open_failed
+    	# Check if file opened successfully
+   	 bltz $s1, file_open_failed
 
-    # Read from the file
-    li $v0, 14
-    move $a0, $s1
-    la $a1, file
-    li $a2, 500  # Adjust buffer length
-    syscall
+    	# Read from the file
+    	li $v0, 14
+    	move $a0, $s1
+    	la $a1, file
+    	li $a2, 500  # Adjust buffer length
+    	syscall
 
-    # Close the file
-    li $v0, 16
-    move $a0, $s1
-    syscall
-    j done
+    	# Close the file
+    	li $v0, 16
+    	move $a0, $s1
+    	syscall
+    	j done
 
 file_open_failed:
 	printStr(fileError)
 	printStr(newLine)
-    j prompt
+    	j prompt
 
 done:
 .end_macro
@@ -54,52 +54,52 @@ done:
 
 
 .macro processKey(%key)
-    process(%key)
-    move $a0, %key
+    	process(%key)
+    	move $a0, %key
 
 convert_loop:
-    # Load a character from the string
-    lb $t1, 0($a0)
+    	# Load a character from the string
+    	lb $t1, 0($a0)
 
-    # Check if the character is null (end of string)
-    beqz $t1, convert_exit
+    	# Check if the character is null (end of string)
+    	beqz $t1, convert_exit
 
-    # Check if the character is a lowercase letter
-    blt $t1, 'a', not_lowercase
-    bgt $t1, 'z', not_lowercase
+    	# Check if the character is a lowercase letter
+    	blt $t1, 'a', not_lowercase
+    	bgt $t1, 'z', not_lowercase
 
-    # Convert lowercase to uppercase (ASCII difference is 32)
-    subi $t1, $t1, 32
-    sb $t1, 0($a0)
+    	# Convert lowercase to uppercase (ASCII difference is 32)
+    	subi $t1, $t1, 32
+    	sb $t1, 0($a0)
 
 not_lowercase:
     # Move to the next character
-    addi $a0, $a0, 1
-    j convert_loop
+    	addi $a0, $a0, 1
+    	j convert_loop
 
 convert_exit:
-    # End of string, return
+    	# End of string, return
 .end_macro
 
 .macro printStr(%str)
-li $v0, 4
-la $a0, %str
-syscall
+	li $v0, 4
+	la $a0, %str
+	syscall
 .end_macro
 
 .macro process(%key)
-    move $t0, %key
-    loop:
+    	move $t0, %key
+loop:
         lb $t1, ($t0)  # Load a character from the buffer
         beqz $t1, done_processing  # Exit loop if null terminator
         beq $t1, 10, replace_newline  # Check for newline (ASCII 10)
         beq $t1, 13, replace_newline  # Check for carriage return (ASCII 13)
-        j continue_loop
-        replace_newline:
-            sb $zero, ($t0)  # Replace newline with null terminator
-        continue_loop:
-            addi $t0, $t0, 1  # Move to the next character
+	j continue_loop
+replace_newline:
+	sb $zero, ($t0)  # Replace newline with null terminator
+continue_loop:
+        addi $t0, $t0, 1  # Move to the next character
         j loop
 
-    done_processing:
+done_processing:
 .end_macro
--- vigenereCipherTest.asm
+++ vigenereCipherTest.asm
@@ -20,102 +20,102 @@ fileError: .asciiz "Error opening the file. Please check the file name and try a
 .text
 main:
 	getFile
-   getKey
-   la $s0, buffer
-   processKey($s0)
-   printStr(newLine)
+   	getKey
+   	la $s0, buffer
+   	processKey($s0)
+   	printStr(newLine)
 
     # Print original plaintext and key
-    li $v0, 4                    # System call for print_str
-    la $a0, file            # Load plaintext address
-    syscall
+    	li $v0, 4                    # System call for print_str
+    	la $a0, file            # Load plaintext address
+    	syscall
+
+    # Print original plaintext and key
+    	li $v0, 4                    # System call for print_str
+    	la $a0, newLine            # Load plaintext address
+    	syscall
+
+    	li $v0, 4                    # System call for print_str
+    	move $a0, $s0                  # Load key address
+    	syscall
     
     # Print original plaintext and key
-    li $v0, 4                    # System call for print_str
-    la $a0, newLine            # Load plaintext address
-    syscall
+    	li $v0, 4                    # System call for print_str
+    	la $a0, newLine            # Load plaintext address
+    	syscall
 
-    li $v0, 4                    # System call for print_str
-    move $a0, $s0                  # Load key address
-    syscall
-    
-    # Print original plaintext and key
-    li $v0, 4                    # System call for print_str
-    la $a0, newLine            # Load plaintext address
-    syscall
-
-    la $a0, file          # Load plaintext address
-    move $a1, $s0                  # Load key address
-    jal encryption                # Call the vigenere function
+    	la $a0, file          # Load plaintext address
+    	move $a1, $s0                  # Load key address
+    	jal encryption                # Call the vigenere function
 
     # Print encrypted plaintext
-    li $v0, 4                    # System call for print_str
-    la $a0, file            # Load encrypted plaintext address
-    syscall
+    	li $v0, 4                    # System call for print_str
+    	la $a0, file            # Load encrypted plaintext address
+    	syscall
 
     # Exit program
-    li $v0, 10                   # System call for exit
-    syscall
+    	li $v0, 10                   # System call for exit
+    	syscall
 
 # Vigenere function
 encryption:
-    move $t0, $a0                # Copy plaintext address
-    move $t1, $a1                # Copy key address
-    li $t2, 0                    # Initialize counter for plaintext index
-    li $t3, 0                    # Initialize counter for key index
+    	move $t0, $a0                # Copy plaintext address
+    	move $t1, $a1                # Copy key address
+    	li $t2, 0                    # Initialize counter for plaintext index
+    	li $t3, 0                    # Initialize counter for key index
 
 loop:
-    lb $t4, ($t0)                # Load a character from plaintext
-    beqz $t4, end		# checks if 
-    blt $t4, 65, skip             # If ASCII value is less than 'A', skip
-    bgt $t4, 122, skip            # If ASCII value is greater than 'z', skip
-    blt $t4, 97, upper            # If ASCII value is less than 'a', it's upper case
-    bgt $t4, 90, lower
+    	lb $t4, ($t0)                # Load a character from plaintext
+    	beqz $t4, end		# checks if 
+    	blt $t4, 65, skip             # If ASCII value is less than 'A', skip
+    	bgt $t4, 122, skip            # If ASCII value is greater than 'z', skip
+    	blt $t4, 97, upper            # If ASCII value is less than 'a', it's upper case
+    	bgt $t4, 90, lower
 
 skip:	
 	addi $t0, $t0, 1
 	j loop                
 lower:
-    lb $t5, ($t1)                # Load a character from key
-    beqz $t5, reset_key          # If null terminator, reset key index
+    	lb $t5, ($t1)                # Load a character from key
+    	beqz $t5, reset_key          # If null terminator, reset key index
 
-    sub $t4, $t4, 97             # Convert plaintext character to 0-25 range
-    sub $t5, $t5, 65             # Convert key character to 0-25 range
+    	sub $t4, $t4, 97             # Convert plaintext character to 0-25 range
+    	sub $t5, $t5, 65             # Convert key character to 0-25 range
 
-    add $t4, $t4, $t5            # Add plaintext and key characters
-    li $t6, 26                   # Constant for modulo operation
-    rem $t4, $t4, $t6            # Modulo 26 to keep within the alphabet range
+    	add $t4, $t4, $t5            # Add plaintext and key characters
+    	li $t6, 26                   # Constant for modulo operation
+    	rem $t4, $t4, $t6            # Modulo 26 to keep within the alphabet range
 
-    add $t4, $t4, 97             # Convert back to ASCII range
+    	add $t4, $t4, 97             # Convert back to ASCII range
 
-    sb $t4, ($t0)                # Store encrypted character back to plaintext
-    addi $t0, $t0, 1             # Move to the next character in plaintext
-    addi $t1, $t1, 1             # Move to the next character in key
-    j loop                       # Repeat for the next character
+    	sb $t4, ($t0)                # Store encrypted character back to plaintext
+    	addi $t0, $t0, 1             # Move to the next character in plaintext
+    	addi $t1, $t1, 1             # Move to the next character in key
+    	j loop                       # Repeat for the next character
 upper:
 
-    lb $t5, ($t1)                # Load a character from key
-    beqz $t5, reset_key          # If null terminator, reset key index
+    	lb $t5, ($t1)                # Load a character from key
+    	beqz $t5, reset_key          # If null terminator, reset key index
 
-    sub $t4, $t4, 65             # Convert plaintext character to 0-25 range
-    sub $t5, $t5, 65             # Convert key character to 0-25 range
+    	sub $t4, $t4, 65             # Convert plaintext character to 0-25 range
+    	sub $t5, $t5, 65             # Convert key character to 0-25 range
 
-    add $t4, $t4, $t5            # Add plaintext and key characters
-    li $t6, 26                   # Constant for modulo operation
-    rem $t4, $t4, $t6            # Modulo 26 to keep within the alphabet range
+    	add $t4, $t4, $t5            # Add plaintext and key characters
+    	li $t6, 26                   # Constant for modulo operation
+    	rem $t4, $t4, $t6            # Modulo 26 to keep within the alphabet range
 
-    add $t4, $t4, 65             # Convert back to ASCII range
+    	add $t4, $t4, 65             # Convert back to ASCII range
 
-    sb $t4, ($t0)                # Store encrypted character back to plaintext
-    addi $t0, $t0, 1             # Move to the next character in plaintext
-    addi $t1, $t1, 1             # Move to the next character in key
-    j loop                       # Repeat for the next character
+    	sb $t4, ($t0)                # Store encrypted character back to plaintext
+    	addi $t0, $t0, 1             # Move to the next character in plaintext
+    	addi $t1, $t1, 1             # Move to the next character in key
+    	j loop                       # Repeat for the next character
 
 reset_key:
-    # Reset key index if end of key is reached
-    move $t1, $s0                  # Reload key address
-    li $t3, 0                    # Reset key index
-    j loop                       # Continue encryption
+    	# Reset key index if end of key is reached
+    	move $t1, $s0                  # Reload key address
+    	li $t3, 0                    # Reset key index
+    	j loop                       # Continue encryption
 
 end:
-    jr $ra                       # Return from function
+	jr $ra                       # Return from function
